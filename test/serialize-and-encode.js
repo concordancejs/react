5e2b62420863ddc7f91c59ebc909b01d4963c2d3
Billy Janitsch <billyjanitsch@gmail.com>
2019-01-12 11:30:18 -0500

    Add failing React.memo test

--- test/serialize-and-encode.js
+++ test/serialize-and-encode.js
@@ -46,6 +46,15 @@ useDeserializedRendered.title = prefix => `deserialized rendered ${prefix} is eq
 const macros = [useDeserialized, useDeserializedRendered]
 
 test('react elements', macros, () => <HelloMessage name='John' />)
+// TODO: Combine next two tests with `macros` array
+test.failing('memoized react elements', useDeserialized, () => {
+  const MemoizedHelloMessage = React.memo(HelloMessage)
+  return <MemoizedHelloMessage name='John' />
+})
+test('memoized react elements', useDeserializedRendered, () => {
+  const MemoizedHelloMessage = React.memo(HelloMessage)
+  return <MemoizedHelloMessage name='John' />
+})
 test('fragments', macros, () => <React.Fragment><HelloMessage name='John' /></React.Fragment>)
 test('object properties', macros, () => {
   return React.createElement('Foo', {object: {baz: 'thud'}})
